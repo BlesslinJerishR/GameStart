2c602a4c615658091586297fad8660c84cd04de2
BlesslinJerishR <mastermindx33@gmail.com>
2022-03-29 10:05:53 +0530

    __test__

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const gstop = document.querySelector('h1');
+const redish = document.querySelector('span');
+
+gstop.addEventListener('click',() => {
+    if(redish.textContent === 'Stop'){
+        redish.textContent = 'Start'
+    }else{
+        redish.textContent = "Stop"
+    }
+});
+
+$(document).ready(function(){
+    $("h1").dblclick(function(e){
+        e.preventDefault();
+    });
+)};
